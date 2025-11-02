814f2f747ce753f9afed9e041bf5246f1cd10db2
Amin Forouzan <amin.pqr@gmail.com>
2025-11-02 13:19:32 +0330

    style(Profile): add timeFrame prop and set buttons text colors based on it
    - set buttons text color based on the timeFrame passed to Profile
    - add Hover effects for buttons when selected or not selected

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,7 @@
-export function Profile() {
+interface ProfileProps {
+  timeFrame: "daily" | "weekly" | "monthly";
+}
+export function Profile({ timeFrame }: ProfileProps) {
   return (
     <article className="w-81.75 rounded-2xl bg-[#1C204B]">
       <div className="flex items-center gap-x-5 rounded-2xl bg-[#5747EA] py-8.5 ps-8 pe-10">
@@ -14,14 +17,23 @@ export function Profile() {
           <p className="text-2xl font-light text-white">Jeremy Robson</p>
         </div>
       </div>
-      <div className="flex justify-between px-8 py-6 text-lg font-normal">
-        <button type="button" className="cursor-pointer text-[#7078C9]">
+      <div className="flex justify-between px-8 py-4 text-lg leading-5.25 font-normal">
+        <button
+          type="button"
+          className={`cursor-pointer rounded-xl px-3 py-2 hover:bg-[#5747EA] ${timeFrame === "daily" ? "text-white" : "text-[#7078C9] hover:text-[#c2c4db]"}`}
+        >
           Daily
         </button>
-        <button type="button" className="cursor-pointer text-white">
+        <button
+          type="button"
+          className={`cursor-pointer rounded-xl px-3 py-2 hover:bg-[#5747EA] ${timeFrame === "weekly" ? "text-white" : "text-[#7078C9] hover:text-[#c2c4db]"}`}
+        >
           Weekly
         </button>
-        <button type="button" className="cursor-pointer text-[#7078C9]">
+        <button
+          type="button"
+          className={`cursor-pointer rounded-xl px-3 py-2 hover:bg-[#5747EA] ${timeFrame === "monthly" ? "text-white" : "text-[#7078C9] hover:text-[#c2c4db]"}`}
+        >
           Monthly
         </button>
       </div>
