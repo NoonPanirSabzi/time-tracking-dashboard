84d1c776af7ed7cbad2fa01a1ba210870e12fdbb
Amin Forouzan <amin.pqr@gmail.com>
2025-11-03 13:12:23 +0330

    feat(App): add static version of App in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Profile, type TimeFrame } from "./components/Profile";
+import { TimeBox } from "./components/TimeBox";
+import data from "./data.json";
 
 function App() {
-  return <></>;
+  const items: React.ReactElement[] = [];
+  const [filter, setFilter] = useState<TimeFrame>("weekly")
   
+  data.forEach((node, index) => {
+    items.push(
+      <TimeBox
+        key={index}
+        activity={node.title}
+        timeFrame={filter}
+        current={node.timeframes[filter].current}
+        previous={node.timeframes[filter].previous}
+      />,
+    );
+  });
+
+  return (
+    <main className="grid mx-auto w-fit gap-x-7.5 gap-y-6 py-20">
+      <Profile timeFrame={filter}/>
+      {items}
+    </main>
+  );
 }
 
-export default App
+export default App;
